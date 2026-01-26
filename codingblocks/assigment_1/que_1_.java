2ab23d4c3a7af489a9b144d6bdd890bebccc8f93
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-01-26 22:45:45 +0530

    first commit

---
+++ codingblocks/assigment_1/que_1_.java
@@ -0,0 +1,26 @@
+
+    // Write a program that takes an integer as input and determines if the number is positive, negative, or zero.
+
+
+    // Input Format
+    // A single integer, N.
+
+
+    // Constraints
+    // -10^9<= N <=10^9
+
+
+    // Output Format
+    // Print "Positive" if the number is greater than 0.
+
+    // Print "Negative" if the number is less than 0.
+
+    // Print "Zero" if the number is exactly 0.
+
+
+public class que_1_ {
+    public static void main(String[] args) {
+        System.out.println("hell");
+    }
+    
+}
---
+++ codingblocks/assigment_1/que_2_.java
@@ -0,0 +1,5 @@
+public class que_2_ {
+    public static void main(String[] args) {
+        System.out.println("hello as1 - que2");
+    }
+}
