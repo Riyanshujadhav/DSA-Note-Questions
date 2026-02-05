490fa2436236858c8c64a922cb93ff23a4812272
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-05 10:40:34 +0530

    fibbonachi

---
+++ class_que/Lecture_11-20/Lecture-15/fibbnachitemp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fibbnachitemp {
+    public static int fibbo(int g){
+            if(g==0||g==1){
+                return g;
+            }else{
+            return fibbo(g-1)+fibbo(g-2);
+            }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int k=0;
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(fibbo(k)+"  ");
+                k++;
+            }System.out.println();
+        }
+}   }
--- class_que/Lecture_11-20/Lecture-15/fibbonachi.java
+++ class_que/Lecture_11-20/Lecture-15/fibbonachi.java
@@ -1,22 +1,20 @@
 
 import java.util.*;
 public class fibbonachi {
-    public static int fibbo(int g){
-            if(g==0||g==1){
-                return g;
-            }else{
-            return fibbo(g-1)+fibbo(g-2);
-            }
+    static int fibbo(int n){
+        
+            if (n==0||n==1){
+                return n;
+            }else 
+                return fibbo(n-1)+fibbo(n-2);
     }
+    
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n= sc.nextInt();
-        int k=0;
-        for(int i=0;i<=n-1;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(fibbo(k)+"  ");
-                k++;
-            }System.out.println();
-        }
+
+        int result =fibbo(n);
+        System.out.println(result);
+        
     }
 }
