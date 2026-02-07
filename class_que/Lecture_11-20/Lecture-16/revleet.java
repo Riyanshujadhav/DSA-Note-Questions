ca3be313dce3b5797c1cd806795765f4887af253
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-07 17:33:07 +0530

    leet_reverse

---
+++ class_que/Lecture_11-20/Lecture-16/revleet.java
@@ -0,0 +1,23 @@
+public class revleet {
+    class Solution {
+    public int reverse(int x) {
+        if(x>Integer.MIN_VALUE/10 || x<Integer.MAX_VALUE/10){
+        int rev=0;
+        while(x!=0){
+            int t=x%10;
+            rev=rev*10+t;
+            x/=10;
+        }
+        return rev;
+        } 
+        else{
+            return 0;
+        }
+    }
+}
+   public static void main(String[] args) {
+    int n=3;
+        reverse r=new reverse();
+      int res=r.reverse(n);
+   } 
+}
