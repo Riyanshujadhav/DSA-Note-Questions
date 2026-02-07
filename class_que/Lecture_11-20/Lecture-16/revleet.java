44bc9d2b9789838b6bce8c6ae60bc9dad42bd567
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-07 17:38:39 +0530

    rev

--- class_que/Lecture_11-20/Lecture-16/revleet.java
+++ class_que/Lecture_11-20/Lecture-16/revleet.java
@@ -17,7 +17,7 @@ public class revleet {
 }
    public static void main(String[] args) {
     int n=3;
-        reverse r=new reverse();
+        reverse r=new reverse(n);
       int res=r.reverse(n);
    } 
 }
