cc46a8eb0efe7cde6c2401d426cabd80fd19d294
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-09 16:16:45 +0530

    a

--- class_que/Lecture_11-20/Lecture-18/que_1.java
+++ class_que/Lecture_11-20/Lecture-18/que_1.java
@@ -1,6 +1,25 @@
 
 public class que_1 {
     public static void main(String[] args) {
-        
+        int []arr= {10,-12,13,20,81};
+       
+int i=0;
+
+         for(int m:arr){
+        //     if(m<=arr[i]){
+        //         m=arr[i];
+                
+        //     }else{
+        //         m=arr[i+1];    
+        //     }
+        //     i++;
+        //     System.out.println(m);
+        // 
+
+        while( m <=arr[i]){
+            m=arr[i];
+            System.out.println(m);
+        }
+        }
     }
 }
