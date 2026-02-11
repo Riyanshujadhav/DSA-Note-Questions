64da58b241b3e5301e25c0243f7951f218770c98
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-11 22:40:33 +0530

    bostan

--- class_que/Lecture_11-20/Lecture-20/bostan_No.java
+++ class_que/Lecture_11-20/Lecture-20/bostan_No.java
@@ -13,7 +13,6 @@ public class bostan_No {
         return sum;
     }
 
-    static
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
