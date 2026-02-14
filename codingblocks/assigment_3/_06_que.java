4dff98ccf77cbc009266da89a56cbb8c0deb5c4c
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-14 21:25:11 +0530

    assigment3

--- codingblocks/assigment_3/_06_que.java
+++ codingblocks/assigment_3/_06_que.java
@@ -5,7 +5,22 @@
 // Write a function which returns the LCM of N1 and N2. Print the value returned
 
 public class _06_que {
-   public static void main(String[] args) {
-       
-   } 
+   public static int gcd(int n1, int n2){
+		while(n2!=0){
+			int temp=n1%n2;
+			n1=n2;
+			n2=temp;
+		}return n1;
+	}
+
+
+    public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int gcd=gcd(n1,n2);
+		int lcm =(n1*n2)/gcd;
+		System.out.println(lcm);
+		
+    }
 }
