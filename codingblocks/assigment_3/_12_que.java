67a22952b8dabe20724ef8505bb7da7b7a79be13
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-14 21:26:00 +0530

    12

--- codingblocks/assigment_3/_12_que.java
+++ codingblocks/assigment_3/_12_que.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class _12_que {
     public static int rev(int n){
-		int rev=0;
+		int rev=1;
 		 while(n!=0){
             int temp=n%10;
 			if(temp==0){
@@ -12,9 +12,17 @@ public class _12_que {
 			n/=10;
 		 }return rev;
 	} 
+	public static int rev2(int n){
+		int rev=0;
+		while (n!=0){
+			int temp=n%10;
+			rev=rev*10+temp;
+			n/=10;
+		}return rev/10;
+	}
     public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println(rev(rev(n)));	
+        System.out.println(rev2(rev(n)));	
     }
 }
