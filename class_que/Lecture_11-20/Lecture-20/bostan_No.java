8798b7c51005990d089251184cf89ced08bad2ce
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-14 22:01:41 +0530

    bostanno

--- class_que/Lecture_11-20/Lecture-20/bostan_No.java
+++ class_que/Lecture_11-20/Lecture-20/bostan_No.java
@@ -3,21 +3,46 @@ import java.util.Scanner;
 
 public class bostan_No {
 
-    static int digitsum(int n)
-    {   int sum=0;
-        while(n!=0){
-            int t=n%10;
-            sum=sum+t;
-            n/=10;
+    public static int digitSum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
         }
         return sum;
     }
+	public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int digiSum=digitsum(n);
-        System.out.println("the sum is:"+digiSum);
-        
+    public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+		if (n <= 1 || isPrime(n)) {
+            System.out.println(0);
+            return;
+        }
+		int originalDigitSum = digitSum(n);
+        int factorDigitSum = 0;
+        int temp = n;
+		for (int i = 2; i * i <= temp; i++) {
+            while (temp % i == 0) {
+                factorDigitSum += digitSum(i);
+                temp /= i;
+            }
+        }
+		if (temp > 1) {
+            factorDigitSum += digitSum(temp);
+        }
+
+        if (originalDigitSum == factorDigitSum)
+            System.out.println(1);
+        else
+            System.out.println(0);
     }
 }
