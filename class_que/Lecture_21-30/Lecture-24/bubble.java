93c121855728c50abe521c821a40acebe6b13151
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-18 16:15:38 +0530

    bubble sort

---
+++ class_que/Lecture_21-30/Lecture-24/bubble.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class bubble {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        // Bubble Sort
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - 1 - i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        // Print Sorted Array
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }
+}
+
+
