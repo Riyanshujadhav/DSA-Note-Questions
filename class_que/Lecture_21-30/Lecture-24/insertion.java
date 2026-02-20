80fa9b9d8f6af01f59c453956f2e41e6390db439
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-21 01:02:36 +0530

    insersion-sorti

--- class_que/Lecture_21-30/Lecture-24/insertion.java
+++ class_que/Lecture_21-30/Lecture-24/insertion.java
@@ -9,6 +9,17 @@ public class insertion {
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
-        
+        for(int i=1;i<arr.length;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0&&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]);
+        }  
     }
 }
