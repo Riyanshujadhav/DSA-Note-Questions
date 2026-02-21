452ab6848ab68a5207eca33eb2a2f5e4a1fa5c7d
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-21 16:27:28 +0530

    selection

--- class_que/Lecture_21-30/Lecture-24/selection.java
+++ class_que/Lecture_21-30/Lecture-24/selection.java
@@ -12,7 +12,7 @@ public class selection {
             arr[i] = sc.nextInt();
         }
 
-        // selection Sort
+        //  selection Sort
         for(int i = 0; i < arr.length ; i++){
             int min=i;
             for(int j = i+1; j < arr.length; j++){
