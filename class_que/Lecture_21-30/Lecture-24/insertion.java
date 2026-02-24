4dc4dbe52c619bfc2c52a1384ef5c81f63d3df6c
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-24 21:41:54 +0530

    insersion

--- class_que/Lecture_21-30/Lecture-24/insertion.java
+++ class_que/Lecture_21-30/Lecture-24/insertion.java
@@ -5,7 +5,6 @@ public class insertion {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
-
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
