3958df4e98d63da1be45786ded76a8f2ec265308
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-25 14:51:07 +0530

    bubble

--- class_que/Lecture_21-30/Lecture-24/bubble.java
+++ class_que/Lecture_21-30/Lecture-24/bubble.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class bubble {
     
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
