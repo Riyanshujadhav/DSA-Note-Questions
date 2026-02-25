0be473dd51857024816890eb38b922529222a4de
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-25 16:07:19 +0530

    4

--- class_que/extraQuestions/que_4_.java
+++ class_que/extraQuestions/que_4_.java
@@ -1,7 +1,7 @@
 public class que_4_ {
     public static void main(String[] args) {
         int n=5;
-        int [][]=n[][];
+       // int [][]arr=n[][];
             for (int i=1;i<=n;i++){
                 for(int j=1;j<=i;j++){
                     if(i==1||j==1){
