3aa7e12792ac74687f21a04e9d4d0afd069b1298
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-26 13:19:46 +0530

    list1D

--- class_que/Lecture_21-30/Lecture-25/alist2d.java
+++ class_que/Lecture_21-30/Lecture-25/alist2d.java
@@ -1,33 +1,25 @@
-// 2d vector-->  vector<vector<int>>a(n,vector<int>(m));
-// 2d array list -->
-import java.util.*;
-public class arraylist2d {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class alist2d {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // Create 2D ArrayList
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
         System.out.print("Enter number of rows: ");
         int rows = sc.nextInt();
         System.out.print("Enter number of columns: ");
         int cols = sc.nextInt();
-        // Initialize rows
         for (int i = 0; i < rows; i++) {
             list.add(new ArrayList<>());
         }
-        // Taking input
-        System.out.println("Enter elements:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 list.get(i).add(sc.nextInt());
             }
         }
-        // Printing 2D ArrayList
         System.out.println("2D ArrayList:");
         for (int i = 0; i < rows; i++) {
             System.out.println(list.get(i));
         }
-        sc.close();
     }
 }
-
-
---
+++ class_que/Lecture_21-30/Lecture-25/list.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class list {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+}
